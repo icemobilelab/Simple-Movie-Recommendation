93574f5248ffb7aaee8e4dba3e085ec62e1195d1
Ricardo de Souza <ricardo@icemobile.com>
2017-03-30 10:58:07 +0200

    Adding assembly library, and a small piece of code to load from the arguments command line.

--- build.sbt
+++ build.sbt
@@ -6,5 +6,8 @@ scalaVersion := "2.11.9"
 
 
 libraryDependencies ++= Seq(
-  "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0"
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0",
+  "ch.qos.logback"                %  "logback-classic"          % "1.1.7"
 )
+
+assemblyJarName in assembly := "recommender.jar"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,6 +4,18 @@ object Main extends App with LazyLogging {
 
   logger.info("Simple Movie Recommendation - v0.1.0")
 
-  
+  if (args.length < 2) {
+    println("Missing arguments. Usage:")
+    println("\tjava -jar recommender.jar <input> <output>\n")
+    println("\t<input>:\tthe filename of the input CSV")
+    println("\t<output>:\tthe filename of the output CSV")
+    System.exit(0)
+  }
+
+  val inputFilename = args(0)
+  val outputFilename = args(1)
+
+  logger.info(s"Input CSV is $inputFilename")
+  logger.info(s"Output CSV is $outputFilename ")
 
 }
